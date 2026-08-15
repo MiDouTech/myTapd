2d44182931182cff28060409dddf09836780b33a
chenjiaying-miduo <chenjy@miduonet.com>
2026-08-15 23:44:58 +0800

    fix: use available fastjson2 dependency

--- ticket-platform/ticket-application/src/main/java/com/miduo/cloud/ticket/application/customfield/CustomFieldApplicationService.java
+++ ticket-platform/ticket-application/src/main/java/com/miduo/cloud/ticket/application/customfield/CustomFieldApplicationService.java
@@ -1,6 +1,6 @@
 package com.miduo.cloud.ticket.application.customfield;
 
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson2.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.miduo.cloud.ticket.common.enums.ErrorCode;
 import com.miduo.cloud.ticket.common.exception.BusinessException;
